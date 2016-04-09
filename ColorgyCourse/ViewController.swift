4ba1334fe67f2f2260f7dda93ce759ddfebd0524
David Lin <yoxisem544@gmail.com>
2016-04-09 09:56:19 +0800

    Initial Commit

---
+++ ColorgyCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/4/9.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
