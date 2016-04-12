6dbf187d454458d11153fcd8c5f308f50fecb7d3
David Lin <yoxisem544@gmail.com>
2016-04-12 20:04:51 +0800

    add apis

---
+++ ChatMeUserInformation.swift
@@ -0,0 +1,87 @@
+//
+//  ChatMeUserInformation.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/4/12.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+final public class ChatMeUserInformation: NSObject {
+	
+	// MARK: - Parameters
+	// required
+	public let status: Int
+	public let avatarURL: String?
+	public let organizationCode: String
+	public let id: String
+	public let name: String
+	// optional
+	public let aboutSchool: String?
+	public let aboutConversation: String?
+	public let aboutPassion: String?
+	public let aboutHoroscope: String?
+	public let aboutHabitancy: String?
+	public let aboutExpertise: String?
+	
+	public override var description: String {
+		return "ChatMeUserInformation: {\n\tstatus -> \(status)\n\tavatarURL -> \(avatarURL)\n\torganizationCode -> \(organizationCode)\n\tid -> \(id)\n\tname -> \(name)\n\taboutSchool -> \(aboutSchool)\n\taboutConversation -> \(aboutConversation)\n\taboutPassion -> \(aboutPassion)\n\taboutHoroscope -> \(aboutHoroscope)\n\taboutHabitancy -> \(aboutHabitancy)\n\taboutExpertise -> \(aboutExpertise)\n}"
+	}
+	
+	// MARK: - Init
+	public convenience init?(json: JSON) {
+		
+		var _status: Int?
+		var _avatarURL: String?
+		var _organizationCode: String?
+		var _id: String?
+		var _name: String?
+		var _aboutSchool: String?
+		var _aboutConversation: String?
+		var _aboutPassion: String?
+		var _aboutHoroscope: String?
+		var _aboutHabitancy: String?
+		var _aboutExpertise: String?
+		
+		_status = json["status"].number?.integerValue
+		_avatarURL = json["avatar_url"].string
+		_organizationCode = json["organization_code"].string
+		_id = json["id"].string
+		_name = json["name"].string
+		_aboutSchool = json["about"]["school"].string
+		_aboutConversation = json["about"]["conversation"].string
+		_aboutPassion = json["about"]["passion"].string
+		_aboutHoroscope = json["about"]["horoscope"].string
+		_aboutHabitancy = json["about"]["habitancy"].string
+		_aboutExpertise = json["about"]["expertise"].string
+		
+		self.init(status: _status, avatarURL: _avatarURL, organizationCode: _organizationCode, id: _id, name: _name, aboutSchool: _aboutSchool, aboutConversation: _aboutConversation, aboutPassion: _aboutPassion, aboutHoroscope: _aboutHoroscope, aboutHabitancy: _aboutHabitancy, aboutExpertise: _aboutExpertise)
+	}
+	
+	private init?(status: Int?, avatarURL: String?, organizationCode: String?, id: String?, name: String?, aboutSchool: String?, aboutConversation: String?, aboutPassion: String?, aboutHoroscope: String?, aboutHabitancy: String?, aboutExpertise: String?) {
+		
+		// required
+		guard status != nil else { return nil }
+		guard id != nil else { return nil }
+		guard organizationCode != nil else { return nil }
+		guard name != nil else { return nil }
+		
+		self.status = status!
+		self.id = id!
+		self.organizationCode = organizationCode!
+		self.name = name!
+		self.avatarURL = avatarURL
+		
+		// optional
+		self.aboutSchool = aboutSchool
+		self.aboutConversation = aboutConversation
+		self.aboutPassion = aboutPassion
+		self.aboutHoroscope = aboutHoroscope
+		self.aboutHabitancy = aboutHabitancy
+		self.aboutExpertise = aboutExpertise
+		
+		super.init()
+	}
+}
--- ColorgyCourse/ColorgyChatAPI.swift
+++ ColorgyCourse/ColorgyChatAPI.swift
@@ -26,10 +26,15 @@ public enum ChatAPIError: ErrorType {
 	case APIUnavailable
 	/// User has no organization code
 	case NoOrganization
-	/// User has no user id
-	case NoUserId
+	/// User has no user uuid
+	case NoUserUUID
 	/// Internal preparation fail, might be uuid generate fail or something, chech inside
 	case InternalPreparationFail
+}
+
+public enum NameStatus: String {
+	case Ok = "ok"
+	case AlreadyExists = "exists"
 }
 
 public protocol ColorgyChatAPIDelegate: class {
@@ -222,7 +227,7 @@ final public class ColorgyChatAPI: NSObject {
 			
 			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
 				self.mainBlock({
-					failure?(error: ChatAPIError.NoUserId, afError: nil)
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
 				})
 				return
 			}
@@ -269,12 +274,568 @@ final public class ColorgyChatAPI: NSObject {
 		}
 	}
 	
+	///檢查使用者暱稱：(simple test passed)
+	///
+	///用途：給 app 一個 web API endpoint 來更檢查使用者暱稱
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/check_name_exists，參數包含使用者的name
+	///2. 回傳json：{ result: 'ok' }或者json：{ result: 'exists' }、狀態皆為200
+	public func checkNameExists(name: String, success: ((status: NameStatus) -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"name": name
+			]
+			
+			self.manager.POST(self.serverURL + "/users/check_name_exists", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				if let response = response {
+					let json = JSON(response)
+					if json["result"].string == NameStatus.Ok.rawValue {
+						self.mainBlock({
+							success?(status: NameStatus.Ok)
+						})
+						return
+					} else if json["result"].string == NameStatus.AlreadyExists.rawValue {
+						self.mainBlock({
+							success?(status: NameStatus.AlreadyExists)
+						})
+						return
+					} else {
+						self.mainBlock({
+							failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+						})
+						return
+					}
+				} else {
+					self.mainBlock({
+						failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+					})
+					return
+				}
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///更新使用者名稱：(simple test passed)
+	///
+	///用途：給 app 一個 web API endpoint 來更新使用者名稱
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/update_name，參數包含使用者的name、userId、 uuid、accessToken
+	public func updateName(name: String, userId: String, success: (() -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"name": name,
+				"userId": userId,
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + "/users/update_name", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				self.mainBlock({
+					success?()
+				})
+				return
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///更新使用者資料：(simple test passed)
+	///
+	///用途：給 app 一個 web API endpoint 來更新使用者的相關訊息（星座，興趣等等）
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/update_about，參數包含使用者的about、userId、 uuid、accessToken
+	public func updateAbout(userId: String, horoscope: String?, school: String?, habitancy: String?, conversation: String?, passion: String?, expertise: String?, success: (() -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"about": [
+					"horoscope": (horoscope ?? ""), //星座
+					"school": (school ?? ""), //學校
+					"habitancy": (habitancy ?? ""), //居住地
+					"conversation": (conversation ?? ""), //想聊的話題
+					"passion": (passion ?? ""), //現在熱衷的事情
+					"expertise": (expertise ?? "") //專精的事情
+				],
+				"userId": userId,
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + "/users/update_about", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				self.mainBlock({
+					success?()
+				})
+				return
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///更新使用者大頭貼，更新使用者的驗證狀態：
+	///
+	///用途：因為資料庫不同，需要在上傳大頭貼後，或者使用者收到信件驗證後跟chat server確認更新的資料
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/update_from_core，參數包含使用者的 uuid、accessToken
+	public func updateFromCore(success: (() -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + "/users/update_from_core", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				self.mainBlock({
+					success?()
+				})
+				return
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///取得使用者資料（是自己）：
+	///
+	///用途：給 app 一個 web API endpoint 來取得使用者的相關訊息（星座，興趣等等）
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/me，參數包括使用者的 uuid, accessToken
+	///2. 回傳使用者的基本資料，包括status, name, about, lastAnswer, avatar_url
+	public func me(success: ((userInformation: ChatMeUserInformation) -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + "/users/me", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				if let response = response {
+					let json = JSON(response)
+					if let userInformation = ChatMeUserInformation(json: json) {
+						self.mainBlock({
+							success?(userInformation: userInformation)
+						})
+						return
+					} else {
+						self.mainBlock({
+							failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+						})
+						return
+					}
+				} else {
+					self.mainBlock({
+						failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+					})
+					return
+				}
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///取得使用者資料（不是自己的使用者）：
+	///
+	///用途：給 app 一個 web API endpoint 來取得使用者的相關訊息（星座，興趣等等）
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/get_user，參數包含使用者的userId
+	///2. 回傳使用者的公開詳細資料，包含使用者的status,name,about,lastAnsweredDate,lastAnswer,avatar_blur_2x_url(預設就是都是最模糊的),organization_code
+	public func getUser(userId: String, success: ((userInformation: ChatUserInformation) -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + "/users/get_user", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				if let response = response {
+					let json = JSON(response)
+					if let userInformation = ChatUserInformation(json: json) {
+						self.mainBlock({
+							success?(userInformation: userInformation)
+						})
+						return
+					} else {
+						self.mainBlock({
+							failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+						})
+						return
+					}
+				} else {
+					self.mainBlock({
+						failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+					})
+					return
+				}
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
+	
+	///舉報使用者：
+	///
+	///用途：給 app 一個 web API endpoint 來舉報使用者
+	///使用方式：
+	///
+	///1. 傳一個http post給/report/report_user，參數包含uuid, accessToken,  userId, targetId, type,reason
+	public func reportUser(userId: String, targetId: String, type: String?, reason: String?, success: (() -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserUUID, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"uuid": uuid,
+				"accessToken": accessToken,
+				"userId": userId,
+				"targetId": targetId,
+				"type": type ?? "",
+				"reason": reason ?? ""
+			]
+			
+			self.manager.POST(self.serverURL + "/report/report_user", parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				self.mainBlock({
+					success?()
+				})
+				return
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
 	
 	
+	///封鎖使用者：
+	///
+	///用途：給 app 一個 web API endpoint 來封鎖使用者，之後在得到聊天室列表將會得不到被封鎖的人。進不去過去的聊天室，也收不到任何聊天室的訊息（單方面）。
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/block_user，參數包含uuid,accessToken, userId,targetId
+	///2. 回傳200即代表已經封鎖
+	public func blockUser(userId: String, targetId: String, success: (() -> Void)?, failure: ((error: ChatAPIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				failure?(error: ChatAPIError.NetworkUnavailable, afError: nil)
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let uuid = ColorgyUserInformation.sharedInstance().userUUID else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoUserId, afError: nil)
+				})
+				return
+			}
+			
+			guard let accessToken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: ChatAPIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let parameters = [
+				"uuid": uuid,
+				"accessToken": accessToken
+			]
+			
+			self.manager.POST(self.serverURL + , parameters: parameters, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				if let response = response {
+					let json = JSON(response)
+					self.mainBlock({
+						success?()
+					})
+					return
+				} else {
+					self.mainBlock({
+						failure?(error: ChatAPIError.FailToParseResult, afError: nil)
+					})
+					return
+				}
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					self.mainBlock({
+						let afError = AFError(operation: operation, error: error)
+						failure?(error: ChatAPIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
 	
 	
+	///取得聊天室列表：(simple test passed)
+	///
+	///用途：給 app 一個 web API endpoint 來封鎖使用者，之後在得到聊天室列表將會得不到被封鎖的人。進不去過去的聊天室，也收不到任何聊天室的訊息（單方面）。
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/get_available_target，參數包含gender,uuid,accessToken,userId,page，page從零開始，0,1,2,3,4,5...一直到回傳為空陣列為止
+	///2. 如果成功，回傳的資料包括id,name, about,lastAnswer,avatar_blur_2x_url,一次會回傳20個
+	class func getAvailableTarget(userId: String, gender: Gender, page: Int, success: (targets: [AvailableTarget]) -> Void, failure: () -> Void) {}
 	
+	///取得最新問題：
+	///
+	///用途：取得最新問題以及最新問題的date，此處的date會用在回答問題的api call中。
+	///使用方式：
+	///
+	///1. 傳一個http get給/questions/get_question
+	///2. 成功的會會回傳最新的問題以及date參數
+	class func getQuestion(success: (date: String?, question: String?) -> Void, failure: () -> Void) {}
 
+	///回答問題：
+	///
+	///用途：回傳當日（或者最新）問題的答案給伺服器，如果不是最新會回傳錯誤。
+	///使用方式：
+	///
+	///1. 傳一個http post給/users/answer_question，參數包含uuid,accessToken, userId,date(format:yyyymmdd),answer
+	class func answerQuestion(userId: String, answer: String, date: String, success: () -> Void, failure: () -> Void) {}
+	
+	///檢查打招呼：(simple test passed)
+	///
+	///用途：檢查是否已經打過招呼。如果有打過回傳打招呼的結果。
+	///使用方式：
+	///
+	///1. 傳一個http post給/hi/check_hi，參數包含的userId,targetId,uuid,accessToken
+	///2. 回傳打招呼的結果，有兩種狀況可以繼續打招呼：(1) 從沒打過招呼 (2) 被拒絕可以再打一次，兩者的結果都是一樣的status 200：{ result: 'ok, you can say hi' }，若是已經(1)打過招呼然後成功過 (2)打過招呼還在等候回應，回傳status 200：{ result: 'already said hi' }
+	class func checkHi(userId: String, targetId: String, success: (canSayHi: Bool, whoSaidHi: String?, chatroomId: String?) -> Void, failure: () -> Void) {}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
