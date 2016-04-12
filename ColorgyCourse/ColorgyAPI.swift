68e6dea89cfcf727e3464f2d4ff1b2cbe4458fb8
David Lin <yoxisem544@gmail.com>
2016-04-12 15:32:42 +0800

    add api chatAvailableOrganization

--- ColorgyCourse/ColorgyAPI.swift
+++ ColorgyCourse/ColorgyAPI.swift
@@ -1777,8 +1777,65 @@ final public class ColorgyAPI : NSObject {
 	}
 	
 	
-	
-	
+	// MARK: - Chat related API
+	/// Can check which school is available to chat feature
+	public func chatAvailableOrganization(organization: String, success: ((isAvailable: Bool) -> Void)?, failure: ((error: APIError, afError: AFError?) -> Void)?) {
+		
+		guard networkAvailable() else {
+			self.mainBlock({
+				self.mainBlock({
+					failure?(error: APIError.NetworkUnavailable, afError: nil)
+				})
+			})
+			return
+		}
+		
+		qosBlock {
+			guard self.allowAPIAccessing() else {
+				self.mainBlock({
+					failure?(error: APIError.APIUnavailable, afError: nil)
+				})
+				return
+			}
+			
+			guard let accesstoken = self.accessToken else {
+				self.mainBlock({
+					failure?(error: APIError.NoAccessToken, afError: nil)
+				})
+				return
+			}
+			
+			let url = "https://colorgy.io:443/api/v1/available_org/\(organization.uppercaseString).json?access_token=\(accesstoken)"
+			
+			guard url.isValidURLString else {
+				self.mainBlock({
+					failure?(error: APIError.InvalidURLString, afError: nil)
+				})
+				return
+			}
+			
+			self.manager.GET(url, parameters: nil, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) in
+				if let response = response {
+					let json = JSON(response)
+					self.mainBlock({
+						success?(isAvailable: json["available"].boolValue)
+					})
+					return
+				} else {
+					self.mainBlock({
+						failure?(error: APIError.FailToParseResult, afError: nil)
+					})
+					return
+				}
+				}, failure: { (operation: NSURLSessionDataTask?, error: NSError) in
+					let afError = AFError(operation: operation, error: error)
+					self.mainBlock({
+						failure?(error: APIError.APIConnectionFailure, afError: afError)
+					})
+					return
+			})
+		}
+	}
 	
 	
 	
