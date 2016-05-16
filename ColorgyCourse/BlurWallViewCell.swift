665f761bce00b7f5a342cb48d320b12dce02a309
David Lin <yoxisem544@gmail.com>
2016-05-16 16:43:47 +0800

    add inline doc to blur wall view cell

--- ColorgyCourse/BlurWallViewCell.swift
+++ ColorgyCourse/BlurWallViewCell.swift
@@ -11,14 +11,20 @@ import WebImage
 
 final public class BlurWallViewCell: UICollectionViewCell {
 	
+	// MARK: - Parameters
+	
+	// MARK: Public
+	/// Set this url and will auto update the image for you
 	public var blurImageURL: String! {
 		didSet {
 			updateImage()
 		}
 	}
 	
+	// MARK: Private
 	private var blurImageView: UIImageView!
 	
+	// MARK: - Init
 	override public init(frame: CGRect) {
 		super.init(frame: frame)
 		
@@ -26,11 +32,18 @@ final public class BlurWallViewCell: UICollectionViewCell {
 		configureGradientLayer()
 	}
 	
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Configuration
+	/// Configure image view to contain the blur image
 	func configureBlurImageView() {
 		blurImageView = UIImageView(frame: self.bounds)
 		addSubview(blurImageView)
 	}
 	
+	/// Configure gradient layer on image view
 	func configureGradientLayer() {
 		let gradient = CAGradientLayer()
 		gradient.frame = bounds
@@ -38,13 +51,12 @@ final public class BlurWallViewCell: UICollectionViewCell {
 		blurImageView.layer.addSublayer(gradient)
 	}
 	
+	/// Update image with the given url.
+	/// Will not update if url is not a valid url string
 	func updateImage() {
 		guard let blurImageURL = blurImageURL else { return }
 		guard let url = blurImageURL.url else { return }
 		blurImageView.setImageWithURL(url, placeholderImage: UIImage(named: "1.png"))
 	}
 	
-	required public init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
 }
