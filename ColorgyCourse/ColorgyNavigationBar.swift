8c1542661e527dd77f6e9655c7a85f33019c91a0
David Lin <yoxisem544@gmail.com>
2016-05-21 14:53:27 +0800

    configure colorgy nav bar

--- ColorgyCourse/ColorgyNavigationBar.swift
+++ ColorgyCourse/ColorgyNavigationBar.swift
@@ -8,14 +8,46 @@
 
 import UIKit
 
-class ColorgyNavigationBar: UIView {
+public class ColorgyNavigationBar: UIView {
+	
+	// MARK: - Parameters
+	private var titleLabel: UILabel!
+	public var title: String? {
+		didSet {
+			titleLabel?.text = title
+			titleLabel?.sizeToFit()
+			centerLabel()
+		}
+	}
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+	// MARK: - Init
+	public init() {
+		super.init(frame: CGRect(origin: CGPointZero, size: CGSize(width: UIScreen.mainScreen().bounds.width, height: 64)))
+		backgroundColor = UIColor.whiteColor()
+		configureTitleLabel()
+	}
+	
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Configuration
+	private func configureTitleLabel() {
+		titleLabel = UILabel()
+		titleLabel.frame.size.height = 21
+		titleLabel.font = UIFont.boldSystemFontOfSize(17)
+		titleLabel.textAlignment = .Center
+		titleLabel.frame.size.width = 0.6 * bounds.width
+		titleLabel.sizeToFit()
+		
+		centerLabel()
+		
+		addSubview(titleLabel)
+	}
+	
+	private func centerLabel() {
+		titleLabel.center.x = bounds.midX
+		titleLabel.center.y = (bounds.height - 20) / 2 + 20
+	}
 
 }
