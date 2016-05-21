e9509257a72a094ce04c1f98d6f1a29b38d76e24
David Lin <yoxisem544@gmail.com>
2016-05-21 11:23:16 +0800

    add search course bar

---
+++ ColorgyCourse/SearchCourseBar.swift
@@ -0,0 +1,28 @@
+//
+//  SearchCourseBar.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/5/21.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+final public class SearchCourseBar: UIView {
+	
+	// MARK: - Parameters
+	private var searchTextField: UITextField!
+	private var cancelButton: UIButton!
+	private var searchIconImageVIew: UIImageView!
+	
+	// MARK: - Init
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Configuration
+}
