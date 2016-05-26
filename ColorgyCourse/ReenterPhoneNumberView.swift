f4a7419256365346400580577a8c28bd21f334c5
David Lin <yoxisem544@gmail.com>
2016-05-26 19:58:55 +0800

    1 pixel diff yoyoyo

--- ColorgyCourse/ReenterPhoneNumberView.swift
+++ ColorgyCourse/ReenterPhoneNumberView.swift
@@ -16,6 +16,9 @@ final public class ReenterPhoneNumberView: UIView {
 	private var cancelButton: UIButton!
 	private var confirmButton: UIButton!
 	
+	private var horizontalSeperatorLine: UIView!
+	private var verticalSeperatorLine: UIView!
+	
 	private let titleFontSize: CGFloat = 18
 	private let subtitleFontSize: CGFloat = 12
 	
@@ -25,6 +28,7 @@ final public class ReenterPhoneNumberView: UIView {
 		configureTitleLabel(title)
 		configureSubtitleLabel(subtitle)
 		configurePhoneNumberTextField()
+		configureSeperatorLine()
 	}
 	
 	// MARK: - Configration
@@ -65,14 +69,33 @@ final public class ReenterPhoneNumberView: UIView {
 		phoneNumberTextField = PhoneNumberTextField()
 		phoneNumberTextField.frame.size = CGSize(width: bounds.width - 24 * 2, height: 38)
 		phoneNumberTextField.frame.origin.x = 24
-		phoneNumberTextField.frame.origin.y = subtitleLabel.frame.midY + subtitleFontSize + 12
+		phoneNumberTextField.frame.origin.y = subtitleLabel.frame.midY + subtitleFontSize + 10
 		phoneNumberTextField.font = UIFont.systemFontOfSize(14)
 		phoneNumberTextField.layer.cornerRadius = 3
 		phoneNumberTextField.layer.borderColor = ColorgyColor.lightGrayContentTextColor.CGColor
 		phoneNumberTextField.layer.borderWidth = 1.0
 		phoneNumberTextField.placeholder = "輸入手機號碼"
+		phoneNumberTextField.tintColor = ColorgyColor.MainOrange
+		phoneNumberTextField.autocorrectionType = .No
+		phoneNumberTextField.autocapitalizationType = .None
+		phoneNumberTextField.keyboardType = .NumberPad
 		
 		addSubview(phoneNumberTextField)
+	}
+	
+	private func configureSeperatorLine() {
+		horizontalSeperatorLine = UIView(frame: CGRect(origin: CGPointZero, size: CGSize(width: bounds.width, height: 1)))
+		horizontalSeperatorLine.backgroundColor = ColorgyColor.TextColor
+		horizontalSeperatorLine.frame.origin.y = phoneNumberTextField.frame.maxY + 24
+		
+		addSubview(horizontalSeperatorLine)
+		
+		verticalSeperatorLine = UIView(frame: CGRect(origin: CGPointZero, size: CGSize(width: 0.5, height: 40)))
+		verticalSeperatorLine.backgroundColor = ColorgyColor.lightGrayContentTextColor
+		verticalSeperatorLine.frame.origin.y = horizontalSeperatorLine.frame.maxY + 1
+		verticalSeperatorLine.center.x = bounds.midX
+		
+		addSubview(verticalSeperatorLine)
 	}
 	
 	private func configureCancelButton() {
