3fa88fc18606ed5246e36ee7de5cae48c95cfb74
David Lin <yoxisem544@gmail.com>
2016-05-26 18:12:40 +0800

    add ReenterPhoneNumberView

---
+++ ColorgyCourse/ReenterPhoneNumberView.swift
@@ -0,0 +1,14 @@
+//
+//  ReenterPhoneNumberView.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/5/26.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+final public class ReenterPhoneNumberView: UIView {
+
+	private var
+}
