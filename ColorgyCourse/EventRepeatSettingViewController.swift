12515669d0649001c1a394120e8a0aa22cb2a2ef
David Lin <yoxisem544@gmail.com>
2016-06-13 11:23:41 +0800

    add event repeat setting view and viewmodel

---
+++ ColorgyCourse/EventRepeatSettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EventRepeatSettingViewController.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/6/13.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+final public class EventRepeatSettingViewController: UIViewController {
+	
+	private var viewModel: EventRepeatSettingViewModel?
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ColorgyCourse/EventRepeatSettingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EventRepeatSettingViewModel.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/6/13.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import Foundation
+
+final public class EventRepeatSettingViewModel {
+	
+}
