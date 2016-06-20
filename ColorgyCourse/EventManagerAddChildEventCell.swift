95e37ebdcb4eb86fcc5cb5d02aab5371f070a2b4
David Lin <yoxisem544@gmail.com>
2016-06-20 13:51:00 +0800

    rename EventManager add child event cell

--- ColorgyCourse/EventManagerAddChildEventCell.swift
+++ ColorgyCourse/EventManagerAddChildEventCell.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-public protocol CreateEventAddChildEventCellDelegate: class {
-	func createEventAddChildEventCellAddChildEventButtonClicked()
+public protocol EventManagerAddChildEventCellDelegate: class {
+	func eventManagerAddChildEventCellAddChildEventButtonClicked()
 }
 
-final public class CreateEventAddChildEventCell: UITableViewCell {
+final public class EventManagerAddChildEventCell: UITableViewCell {
 	
 	@IBOutlet weak var addDateButton: UIButton!
-	public weak var delegate: CreateEventAddChildEventCellDelegate?
+	public weak var delegate: EventManagerAddChildEventCellDelegate?
 	
 	override public func awakeFromNib() {
 		super.awakeFromNib()
@@ -27,12 +27,12 @@ final public class CreateEventAddChildEventCell: UITableViewCell {
 		addDateButton.layer.borderColor = ColorgyColor.MainOrange.CGColor
 		addDateButton.layer.borderWidth = 1.5
 		addDateButton.setTitle("繼續新增時段", forState: UIControlState.Normal)
-		addDateButton.addTarget(self, action: #selector(CreateEventAddChildEventCell.addChildEventButtonClicked), forControlEvents: UIControlEvents.TouchUpInside)
+		addDateButton.addTarget(self, action: #selector(EventManagerAddChildEventCell.addChildEventButtonClicked), forControlEvents: UIControlEvents.TouchUpInside)
 		
 		backgroundColor = UIColor.clearColor()
 	}
 	
 	@objc private func addChildEventButtonClicked() {
-		delegate?.createEventAddChildEventCellAddChildEventButtonClicked()
+		delegate?.eventManagerAddChildEventCellAddChildEventButtonClicked()
 	}
 }
