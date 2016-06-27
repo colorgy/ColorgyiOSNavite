8707afc32108d60b68232331c4452aaa1234d7a2
David Lin <yoxisem544@gmail.com>
2016-06-27 11:04:00 +0800

    add inline doc to colorgyconfig

--- ColorgyCourse/ColorgyConfig.swift
+++ ColorgyCourse/ColorgyConfig.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 
+/// This is for server settings.
+/// Store server url, client id, client secret and so on here.
 public struct ColorgyConfig {
+	/// Server url, now is at "http://staging.colorgy.io"
+	/// Need to change when in production mode.
 	static let serverURL: String = "http://staging.colorgy.io"
+	/// Colorgy app id.
 	static let clientID: String = "3e1f5097e78ff964223903a3c9da891df61917e950feada039ae81f99c8f8aef"
+	/// Colorgy app secret.
 	static let clientSecret: String = "18ba15c6579584545b6947749b80ccf2922aca97eefa501236ee36e5b49bdd71"
 }
