0165d1ec7939d97e3055c72ce231591206088c5f
David Lin <yoxisem544@gmail.com>
2016-06-29 16:31:49 +0800

    setup settings nib

---
+++ ColorgyCourse/SettingsSwitchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsSwitchCell.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/6/29.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+final public class SettingsSwitchCell: UITableViewCell {
+
+	@IBOutlet weak var seperatorLine: UIView!
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var switchControl: UISwitch!
+	
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+		
+		selectionStyle = .None
+    }
+
+}
