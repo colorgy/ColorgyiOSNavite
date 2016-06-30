83692796358f10e7fc05e4b7f3a876fc3c415c80
David Lin <yoxisem544@gmail.com>
2016-06-30 10:50:58 +0800

    adding notification settings vc

---
+++ ColorgyCourse/NotificationSettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationSettingsViewController.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/6/30.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+final public class NotificationSettingsViewController: UIViewController {
+	
+	// MARK: - Parameters
+	private var notificationSettingsTableView: UITableView!
+
+	// MARK: - Life Cycle
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+	// MARK: - Configuration
+	private func configureNotificationSettingsTableView() {
+		
+	}
+}
