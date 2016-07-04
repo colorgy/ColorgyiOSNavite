4c0884134fe692f861da2973ba97ff285c12a1c9
David Lin <yoxisem544@gmail.com>
2016-07-04 11:40:59 +0800

    add realm object

--- ColorgyCourse/SubeventRealmObject.swift
+++ ColorgyCourse/SubeventRealmObject.swift
@@ -1,5 +1,5 @@
 //
-//  EventRealmObject.swift
+//  SubeventRealmObject.swift
 //  ColorgyCourse
 //
 //  Created by David on 2016/7/4.
@@ -9,15 +9,15 @@
 import Foundation
 import RealmSwift
 
-public class EventRealmObject: Object {
-	
+public class SubeventRealmObject: Object {
+    
 	dynamic var id: String = ""
 	dynamic var name: String = ""
+	dynamic var uuid: String =  ""
+	dynamic var detailDescription: String?
 	dynamic var rrule: String?
 	dynamic var dtStart: String = ""
 	dynamic var dtEnd: String = ""
-	dynamic var detailDescription: String?
-	dynamic var referenceId: String?
 	dynamic var createdAt: String = ""
 	dynamic var updatedAt: String?
 	
